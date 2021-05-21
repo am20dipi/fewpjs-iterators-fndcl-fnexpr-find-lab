d9e221b0890fb6cec0ccd17e973a00c03cfe1a1b
Adriana <am20dipi@alum.siena.edu>
2021-05-21 18:27:07 -0400

    commit tree 365514d9681bbec708f19e0ad4b4095b186795bd

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "1969", result: "W"},
+    {year: "2016", result: "N/A"}
+]
 
+function superbowlWin(record) {
+    let result = record.find(record => record.result === "W")
+    return result ? result.year : undefined
+}
